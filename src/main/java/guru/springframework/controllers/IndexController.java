17faabc430e6a60e9d4a7ab924a23da7db87b5e4
r0s0bbo <Ragini.Sharma@walmart.com>
2021-07-27 19:21:18 +0530

    printed hey

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -9,8 +9,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
+
+
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage(){
+
+        System.out.println("hey");
         return "index";
     }
 }
